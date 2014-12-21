f29d6ab8be0595fd7b6b2ae1fe3eb7db266bea28
nah <poop@you.com>
2014-12-21 07:17:44 +0000

    Update sikuli to work on fixed-size screen.

--- maploop.sikuli/maploop.py
+++ maploop.sikuli/maploop.py
@@ -1,8 +1,16 @@
-for i in range(1, 100):
+import time
+
+i = 0
+while True:
+    i += 1
     print "Round %d" % i
-    click("1419126965852.png")
+
+    try:
+        click("1419142568536.png")
+    except:
+        print "Warning: Could not setup game!"
     
-    bots = "1419127025186.png"
+    bots = "1419142606501.png"
     try:
         # If we can't find this, someone
         # probably joined our game.
@@ -12,38 +20,27 @@ for i in range(1, 100):
         print "Warning: Not playing with bots."
 
     try:
-        weaponSelect = "1419127084335.png"
-        wait(weaponSelect, 30)
+        weaponSelect = "1419143335114.png"
+        wait(weaponSelect, 60)
         click(weaponSelect)
             
-        weapon = "1419127238009.png"
+        weapon = "1419142807236.png"
         click(weapon)
+        
+        keyDown(Key.LEFT)
+        time.sleep(1.0)
+        keyUp(Key.LEFT)
     except:
         print "Warning: Not our turn?"
-    
-    
-    # Fire the weapon
-    # "1419128394594.png""1419133463444.png""1419133473651.png" 
-    left = "1419136051796.png"
-    right = "1419133632083.png"
-    space = "1419127409411.png"
-    if exists(left):
-        dragDrop(left, space)
-    elif exists(right):
-        dragDrop(right, space)
-    else:
-        # If we can't find an image to shoot,
-        # just let the other player go
-        # rather than crashing.
-        print "Warning: Could not shoot."
-    
-    back = "1419127342958.png"
-    # This one can take a while
-    wait(back, 120)
-    
-    # sikuli finds this button while it's still
-    # disabled, wait a bit for it to become
-    # enabled before clicking.
-    import time
-    time.sleep(3)
-    click(back)
+        
+    try:
+        back = Pattern("1419142726522.png").similar(0.9)
+        # This one can take a while
+        wait(back, 60*4)
+        # sikuli finds this button while it's still
+        # disabled, wait a bit for it to become
+        # enabled before clicking.
+        time.sleep(3)
+        click(back)
+    except:
+        print "Warning: Could not go back!"
